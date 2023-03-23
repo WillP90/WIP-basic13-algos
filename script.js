f36d5ac3d29e4d51f85aee9ba34b549235c818e9
WillP90 <william.pearce000@gmail.com>
2023-03-23 16:48:15 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("hello world");
+
+// #1 Print 1-255
+// printall the integers from 1 to 255
+
+function printAll(){
+    // for loop to print the variable i 255 times
+    for(i = 1; i <= 255; i++){
+        console.log(i);
+    }
+}
+printAll()
