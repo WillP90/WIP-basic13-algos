26eeaa9038fcacc389f19aac5b96b4f799ddc4bc
WillP90 <william.pearce000@gmail.com>
2023-03-23 17:01:17 -0700

    second

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ function printAll(){
     }
 }
 printAll()
+
+// #2 Print Sum 0-255
+// print integers from 0 to 255, and with each integer print the sum so far
+
+function printSum(){
+    var sum = 0
+    for(i = 1; i <= 255; i++){
+        console.log(i);
+        sum = sum + i
+        console.log("The sum so far " + sum);
+    }
+}
+printSum()
